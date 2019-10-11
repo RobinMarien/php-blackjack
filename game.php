42549244af18996f836a64b75bdabf47a15d3096
RobinMarien <dev.robinmarien@gmail.com>
2019-10-11 15:29:29 +0200

    update

--- game.php
+++ game.php
@@ -25,6 +25,11 @@ if (isset($_REQUEST['btn_submit'])) {
 
         if ($Player->getScore() > 21){
             echo "You lost";
+            session_destroy();
+        }
+        else if ($Player->getScore() == 21) {
+            echo "Blackjack! You won";
+            session_destroy();
         }
     }
 
@@ -36,19 +41,20 @@ if (isset($_REQUEST['btn_submit'])) {
         while
             ($Dealer->getScore() < 15);
 
-        if ($Dealer->getScore())
-    }
+        if ($Dealer->getScore() > 21){
+            echo "You won";
+            session_destroy();
+        }
 
-    if ($_REQUEST['btn_submit'] == "SURRENDER") {
-        $Player->hit();
-        $_SESSION["Player"] = $Player->getScore();
-
-        if ($Player->getScore() > 21){
-            echo "You lost";
+        else if (($Player->getScore()) > ($Dealer->getScore())){
+            echo "You won";
+            session_destroy();
         }
     }
 
-
+    if ($_REQUEST['btn_submit'] == "SURRENDER") {
+        session_destroy();
+        }
 }
 
 ?>
@@ -67,10 +73,27 @@ if (isset($_REQUEST['btn_submit'])) {
         <title>Blackjack</title>
     </head>
     <body>
+
+    <div class="container text-center">
+        <div class="col-md-12">
+            <h1>BLACKJACK</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <h3>PLAYER</h3>
+                <p></p>
+            </div>
+            <div class="col-md-6">
+                <h3>DEALER</h3>
+
+            </div>
+        </div>
+
+    </div>
     <form action="game.php" method="POST">
-        <input type="submit" name="btn_submit" value="HIT"/>
+        <input type="submit" name="btn_submit" value="HIT">
         <input type="submit" name="btn_submit" value="STAND"/>
-        <input type="submit" name="btn_submit" value="SURRENDER"/>
+        <input type="submit" name="btn_submit" value="SURRENDER">
     </form>
     </body>
     </html>
